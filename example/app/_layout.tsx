a15f3aecd9e2dc9102700bc8f1e1cebe1272788d
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-05-03 15:05:11 +0200

    feat(documentation): added drawer layout + manage status for components in misc page
    Setup app so that it uses a drawer layout to show the components.

--- example/app/_layout.tsx
+++ example/app/_layout.tsx
@@ -1,5 +1,44 @@
-import { Stack } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { Platform, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  const [isSkiaReady, setIsSkiaReady] = useState(Platform.OS !== 'web');
+
+  useEffect(() => {
+    if (Platform.OS === 'web') {
+      setInterval(() => {
+        setIsSkiaReady(true);
+      }, 100);
+    }
+  }, []);
+
+  if (!isSkiaReady) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer
+        screenOptions={{
+          swipeEnabled: Platform.OS !== 'web',
+        }}
+        detachInactiveScreens
+      >
+        <Drawer.Screen
+          name="index"
+          options={{ drawerLabel: 'Home', title: 'overview' }}
+        />
+        <Drawer.Screen
+          name="misc"
+          options={{ drawerLabel: 'Miscellaneus', title: 'miscellaneus' }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
+  );
 }
